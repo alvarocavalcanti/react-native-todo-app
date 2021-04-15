9d59c1a4eb80748c80b7503e4a50e4c4fe77c331
Alvaro Cavalcanti <alvaro.cavalcanti@wpengine.com>
2021-04-15 18:47:43 +0100

    feat(App): :sparkles: Added validation and keyboard dismissal

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { Alert, FlatList, TouchableWithoutFeedback, Keyboard, StyleSheet, Text, View } from 'react-native';
+import AddTodo from './components/AddTodo';
 import Header from './components/Header';
 import TodoItem from './components/TodoItem';
 
@@ -10,29 +11,47 @@ export default function App() {
     {text: 'buy butter', key: '3'},
   ]);
 
-  const [foo, setFoo] = useState('');
-
   const pressHandler = (key: string) => {
     setTodos((prevTodos) => {
       return prevTodos.filter(todo => todo.key != key)
     });
   };
 
+  const submitHandler = (text: string) => {
+    if (text.length > 3) {
+      setTodos((prevTodos) => {
+        return [
+          { text: text, key: Math.random().toString()},
+          ...prevTodos
+        ]
+      });
+    } else {
+      Alert.alert('OOPSY!', 'Todos must be over 3 chars long', [
+        {text: 'Understood', onPress: () => console.log('alert closed')}
+      ]);
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Header />
-      <View style={styles.content}>
-        <View style={styles.list}>
-          <Text>{ foo }</Text>
-          <FlatList
-           data={todos}
-           renderItem={({ item }) => (
-             <TodoItem item={ item } pressHandler={ pressHandler } />
-           )}
-          />
+    <TouchableWithoutFeedback onPress={() => {
+      Keyboard.dismiss();
+      console.log('keyboard dismissed');
+    }}>
+      <View style={styles.container}>
+        <Header />
+        <View style={styles.content}>
+        <AddTodo submitHandler={ submitHandler }/>
+          <View style={styles.list}>
+            <FlatList
+            data={todos}
+            renderItem={({ item }) => (
+              <TodoItem item={ item } pressHandler={ pressHandler } />
+            )}
+            />
+          </View>
         </View>
       </View>
-    </View>
+    </TouchableWithoutFeedback>
   );
 }
 
